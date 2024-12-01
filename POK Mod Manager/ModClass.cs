1d936199056078281ce7a037d028b3077a3e5103
DolfeLive <115967108+DolfeLive@users.noreply.github.com>
2024-12-01 14:46:39 +1100

    Update ModClass.cs

--- POK Mod Manager/ModClass.cs
+++ POK Mod Manager/ModClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,38 +15,47 @@ namespace POKModManager
         /// is the mod enabled?
         /// </summary>
         public bool Enabled = true;
+
         /// <summary>
         /// Run stuff in the menu?
         /// </summary>
         public bool RunUpdateOnMenu = false;
+
         /// <summary>
         /// runs when the mod is enabled
         /// </summary>
         public virtual void OnEnabled() { }
+
         /// <summary>
         /// runs when the mod is disabled
         /// </summary>
         public virtual void OnDisabled() { }
+
         /// <summary>
         /// runs whenever a scene is started
         /// </summary>
         public virtual void SceneChange(int sceneIndex) { }
+
         /// <summary>
         /// update for GUI
         /// </summary>
         public virtual void GUIUpdate() { }
+
         /// <summary>
         /// start is called in the start
         /// </summary>
         public virtual void Start() { }
+
         /// <summary>
         /// update is ran every frame
         /// </summary>
         public virtual void Update(float deltaTime) { }
+
         /// <summary>
         /// fixedupdate is ran for physics
         /// </summary>
         public virtual void FixedUpdate(float deltaTime) { }
+
         /// <summary>
         /// prints a message, wrapper for the manager printer
         /// </summary>
@@ -54,6 +63,7 @@ namespace POKModManager
         {
             POKManager.printInfo(obj, GetType().Name);
         }
+
         /// <summary>
         /// prints a warning message, wrapper for the manager printer
         /// </summary>
@@ -61,6 +71,7 @@ namespace POKModManager
         {
             POKManager.printInfo(obj, GetType().Name);
         }
+
         /// <summary>
         /// prints a error message, wrapper for the manager printer
         /// </summary>
